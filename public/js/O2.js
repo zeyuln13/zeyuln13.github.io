495ee7d45e30b488b2dfbedcf8224f070e9992ad
zeyuln13 <0213shoran@gmail.com>
2022-11-17 01:29:35 +0800

    11.17.3

--- public/js/O2.js
+++ public/js/O2.js
@@ -35,3 +35,42 @@ splitScroll();
 //     })
 //     .setClassToggle(".redBlock", "show")
 //     .addTo(controller);
+
+
+const sections = document.querySelectorAll(".about-pages");
+const navLi = document.querySelectorAll("section nav>ul>li");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  })
+})
+
+// *********************
+// This Code is for only the floating card in right bottom corner
+// **********************
+
+const WebCifarIcon = document.querySelector("#webCifar-icon");
+const WebCifarEl = document.querySelector("#webCifar");
+const close = WebCifarEl.querySelector(".close");
+const youtubeLink = document.querySelector(".youtubeLink");
+
+WebCifarIcon.addEventListener("click", () => {
+  WebCifarEl.classList.add("active");
+});
+close.addEventListener("click", () => {
+  WebCifarEl.classList.remove("active");
+});
+
+youtubeLink.setAttribute("href", "https://youtu.be/RsPWEmfOQdU");
--- public/js/dist/O2.dev.js
+++ public/js/dist/O2.dev.js
@@ -29,3 +29,38 @@ splitScroll(); // var controller = new ScrollMagic.Controller();
 //     })
 //     .setClassToggle(".redBlock", "show")
 //     .addTo(controller);
+
+var sections = document.querySelectorAll(".about-pages");
+var navLi = document.querySelectorAll("section nav>ul>li");
+window.addEventListener("scroll", function () {
+  var current = "";
+  sections.forEach(function (section) {
+    var sectionTop = section.offsetTop;
+    var sectionHeight = section.clientHeight;
+
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+  navLi.forEach(function (li) {
+    li.classList.remove("active");
+
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+}); // *********************
+// This Code is for only the floating card in right bottom corner
+// **********************
+
+var WebCifarIcon = document.querySelector("#webCifar-icon");
+var WebCifarEl = document.querySelector("#webCifar");
+var close = WebCifarEl.querySelector(".close");
+var youtubeLink = document.querySelector(".youtubeLink");
+WebCifarIcon.addEventListener("click", function () {
+  WebCifarEl.classList.add("active");
+});
+close.addEventListener("click", function () {
+  WebCifarEl.classList.remove("active");
+});
+youtubeLink.setAttribute("href", "https://youtu.be/RsPWEmfOQdU");
